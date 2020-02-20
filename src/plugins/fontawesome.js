a01d9ac1e85b236dd8fea5c78402df496a38e69c
zeeker <zeekerpro@gmail.com>
2020-02-20 12:44:51 +0800

    font-awesome

---
+++ src/plugins/fontawesome.js
@@ -0,0 +1,11 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+
+library.add(fas, far, fab)
+
+Vue.component('fa-icon', FontAwesomeIcon)
+Vue.component('fa-layers', FontAwesomeLayers)
+Vue.component('fa-layers-text', FontAwesomeLayersText)
