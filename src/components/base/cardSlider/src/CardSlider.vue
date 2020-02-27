ce887a1a7561526f8fe58c0d19b6d8d843e04878
zeeker <zeekerpro@gmail.com>
2020-02-27 17:13:36 +0800

    card slider

--- src/components/base/cardSlider/src/CardSlider.vue
+++ src/components/base/cardSlider/src/CardSlider.vue
@@ -2,22 +2,33 @@
 <div class="card-slider">
 
 	<!-- slide control button -->
-	<div class="control prev">
+	<div 
+		@click="slide('prev')"
+		class="control prev">
 		<button class="btn-icon">
-			<span class="iconfont"></span>
+			<fa-icon :icon="['fas', 'angle-left']" size="lg" mask="circle" fixd-width></fa-icon>
 		</button>
 	</div>
-	<div class="control next">
+	<div 
+		@click="slide('next')"
+		class="control next">
 		<button class="btn-icon">
-			<span class="iconfont"></span>
+			<fa-icon :icon="['fas', 'angle-right']" size="lg" mask="circle" fixd-width></fa-icon>
 		</button>
 	</div>
 
 	<!-- slider content-->
-	<div class="slider-content">
-		<ul>
-			<li v-for="item in items"></li>
-		</ul>
+	<div class="slider-content" :style="{overflowX: isSliding ? 'hidden' : ''}">
+		<!-- slide-items -->
+		<div v-for="(item, index) in items" 
+				 :style="{ 
+						width: itemWidth + '%', 
+						left: slidedLength + '%', 
+						visibility: isSliding || itemsVisible[index] ? 'visible' : 'hidden',
+				 }"
+				 class="slide-item">
+			<slot :item="item"></slot>
+		</div>
 	</div>
 
 </div>
@@ -37,15 +48,113 @@ export default {
 		},
 		interval: {
 			type: Number,
-			default: 1000 // 1s
+			default: 1000 // 默认滑动间隔1s
 		},
 		startItemIndex: {
 			type: Number,
 			default: 0 // 默认显示第一个item
+		}
+	},
+	data(){
+		return {
+			startShowIndex: 0,
+			slidedLength: 0,
+			isSliding: false,
+		}
+	},
+	computed: {
+		itemWidth: function(){
+			return 100 / this.showCount;
+		},
+		slideableLength: function(){
+			return (this.items.length - this.showCount) * this.itemWidth;
+		},
+		itemsVisible: function(){
+			return new Array(this.items.length).fill(false).map((current, index) =>(index >= this.startShowIndex)  && (index < this.startShowIndex + this.showCount));
+		}
+	},
+	methods: {
+		async slide(direction="next"){
+			// 计算滑动距离
+			let s = 0;
+			if(direction == "prev"){
+				if(this.startShowIndex > 0){
+					s = this.itemWidth;
+					this.startShowIndex -= 1;
+				}
+			}else {
+				if(this.startShowIndex < (this.items.length - this.showCount)){
+					s = - this.itemWidth;
+					this.startShowIndex += 1;
+				}
+			}
+
+			this.isSliding = true;
+			let res = await this.startMove(s);
+			this.isSliding = false;
+			
+		},
+		startMove: function(s){
+			// 计算速度
+			let t = this.interval;
+			let v = s * 30 / t;
+
+			let task = new Promise((resolve, reject) => {
+				let intervalTimer = setInterval(() => {
+					if(Math.abs(s) > Math.abs(v)){
+						s -= v;
+						this.slidedLength += v;
+					}else{
+						this.slidedLength += s;
+						clearInterval(intervalTimer);
+						resolve();
+					}
+				}, 30);
+			})
+			
+			return task;
 		}
 	}
 }
 </script>
 
 <style lang="scss" scoped>
+.card-slider {
+	width: 100%;
+	height: 100%;
+	position: relative;
+	overflow-x: hidden;
+	.control {
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		cursor: pointer;
+		z-index: 100;
+		button {
+			border: none;
+			outline: none;
+			font-size: 24px;
+			color: #333;
+			background: transparent;
+			opacity: 0.3;
+		}
+		&.prev {
+			left: 0;
+		}
+		&.next { 
+			right: 0;
+		}
+	}
+	
+	.slider-content {
+		margin: 0 auto;
+		@include flex-box($justify: flex-start);
+	}
+
+	.slide-item {
+		flex-shrink: 0;
+		position: relative;
+		overflow: hidden;
+	}
+}
 </style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,15 +3,49 @@
 		default layout
 		<router-view>
 		</router-view>
-		<card-slider></card-slider>
+		<div class="slider">
+			<card-slider :items="items" :interval=1000>
+				<template v-slot:default="{item}">
+					<div class="card">
+						{{ item.a }}
+					</div>
+				</template>
+			</card-slider>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	name: "DefaultLayout"
+	name: "DefaultLayout",
+	data(){
+		return {
+			items: [
+				{a: 1},
+				{a: 2},
+				{a: 3},
+				{a: 4},
+				{a: 5},
+				{a: 6},
+				{a: 7},
+				{a: 8}
+			]
+		}
+	}
 }
 </script>
 
 <style lang="scss">
+
+
+.slider {
+	margin: 0 auto;
+	width: 1020px;
+}
+
+.card {
+	height: 500px;
+	border: 1px solid #ccc;
+}
+
 </style>
